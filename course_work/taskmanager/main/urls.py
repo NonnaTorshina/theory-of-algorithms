fe43edabfc3f4cd1049e4f1435cdbb35e62d022d
NonnaTorshina <nonnatorshina@mail.ru>
2025-11-03 11:08:42 +0300

    добавлен url tsp для применения алгоритма для решения задачи

--- course_work/taskmanager/main/urls.py
+++ course_work/taskmanager/main/urls.py
@@ -3,7 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('tsp-algorythm', views.tsp_algorythm, name='tsp-algorythm'),
-    path('history', views.history, name='history')
-
-
+    path('history', views.history, name='history'),
+    path('tsp/', views.tsp, name='tsp'),
 ]
